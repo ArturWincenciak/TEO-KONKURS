69d48d2c23cdf030a65703c0a1b34e8e43e16f1f
Artur Wincenciak <artur.wincenciak@pirios.com>
2017-06-06 00:06:35 +0200

    remove comment for testing CI in VSTS

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -10,7 +10,6 @@ namespace BenchmarkTeoVincent
     {
         static void Main(string[] args)
         {
-            // change for test CI with VSTS
             //for (int i = 0; i < 10; i++) // uncomment and fire up the same test many times
             {
                 Summary summary = BenchmarkRunner.Run<CtiTransformatorTester>();
